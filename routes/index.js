a94a68e851e0a68a633c545a8ce437aab58982b2
Marco Beltempo <marco.beltempo@gmail.com>
2017-11-23 12:38:53 -0500

    Adding Travis CI along with lint tests

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var fs = require('fs');
+var fs = require("fs");
 var filesize = require("filesize");
-const path = require('path');
-var file = './test_files/sample_file.txt';
-
+var path = require("path");
+var file = "./test_files/sample_file.txt";
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-
-  fs.readFile(file, 'utf8', function (err, data) {
+router.get("/", function(req, res, next) {
+  fs.readFile(file, "utf8", function(err, data) {
     if (err) {
       return console.log(err);
     } else {
+      fs.stat(file, function(err, stats) {
+        let fileStats = [
+          "Ananalyzing file from " + file,
+          "Directory: " + path.win32.dirname(file), // gets the files directroy
+          "Filename: " + path.win32.basename(file), // gets file name
+          "Extension: " + path.win32.extname(file), // gets file extension
+          "Size:" +
+            stats.size +
+            " Bytes" +
+            " " +
+            getKilobytes(stats.size) +
+            " Kilobytes", // gets file size and converst to readable bytes
+          "File contents: " + data // contents in the file
+        ];
 
-      fs.stat(file, function (err, stats) {
-        var fileStats = [
-          'Ananalyzing file from ' + file,
-          'Directory: ' + path.win32.dirname(file), //gets the files directroy 
-          'Filename: ' + path.win32.basename(file), //gets file name
-          'Extension: ' + path.win32.extname(file), //gets file extension
-          'Size:' + stats.size + ' Bytes' + ' ' + getKilobytes(stats.size) + ' Kilobytes',        //gets file size and converst to readable bytes
-          'File contents: ' + data                  //contents in the file
-        ]
-
-        res.render('index', {
-          title: 'Fileside',
+        res.render("index", {
+          title: "Fileside",
           stats: fileStats
         });
-        //For debugging
+        // For debugging
         console.log("Directory: ", path.win32.dirname(file));
         console.log("Filename: ", path.win32.basename(file));
         console.log("Extension: ", path.win32.extname(file));
-        console.log('Size:', filesize(stats["size"]));
+        console.log("Size:", filesize(stats["size"]));
         console.log("File contents: ", data);
       });
     }
-
   });
-
 });
 
 function getKilobytes(bytes) {
-  
-  var convertToKiloBytes = bytes / 1024
-  var kilobytes = convertToKiloBytes.toFixed(2)
+  let convertToKiloBytes = bytes / 1024;
+  let kilobytes = convertToKiloBytes.toFixed(2);
 
   return kilobytes;
 }
