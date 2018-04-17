44b3385502e6188bc334704bd948f50d2357766f
Abdul Kabia <akkabia@gmail.com>
2018-04-17 13:21:26 -0400

    Made upload page and redirects

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var fs = require("fs");
 var express = require("express");
 var router = express.Router();
 var fileside = require("../src/fileside.js");
+var multer = require("multer");
+var upload = multer({ dest: "../uploads/" });
 
 var testFilePath = "./test/test_file.txt";
 var algorithms = ["md5", "sha1", "sha256", "sha512"];
@@ -42,6 +44,25 @@ router.get("/", function(req, res, next) {
     stats: arr1
   });
   arr1 = [];
+});
+
+router.post("/upload", upload.single("userFile"), function(req, res, next) {
+  var userMessage = "";
+  if (req.file) {
+    userMessage = "File successfully recieved";
+  } else {
+    userMessage = "File was not recieved";
+  }
+  res.render("upload", {
+    title: "Fileside",
+    stats: arr1,
+    message: userMessage
+  });
+  arr1 = [];
+});
+
+router.get("/upload", function(req, res, next) {
+  res.render("upload");
 });
 
 module.exports = router;
